ec1b1979f4d74512dbbd6a85275b30ca0fd624d0
MeMeLwin <lwin.meme993@gmail.com>
2018-12-14 16:44:58 +0630

    api usage

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
-const API_KEY = "8b0fec272e8a0dbbafd6d2add723d447";
+const API_KEY = "8b0fec272e8a0dbbafd6d2add723d447";8
 
 class App extends React.Component {
+
+  getWeather=async(e)=>{
+    e.preventDefault();
+    const api_call=await fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${API_KEY}&units=metric');
+    const data=await api_call.json();
+    console.log(data);
+
+  }
   render() {
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather}/>
         <Weather />
       </div>
     );
